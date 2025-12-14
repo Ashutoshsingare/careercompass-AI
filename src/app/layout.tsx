f764537e8944fe03a9976aa64a946d8eb1d466e3
Ashutosh singare <ashutoshsingare05@gmail.com>
2025-12-14 17:56:11 +0000

    can you change the color of Browser Tab Logo to same color like navbar l

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const faviconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="hsl(207 90% 68%)"><path d="m12 2-7 20 7-4 7 4-7-20zM12 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/></svg>`;
+  const faviconDataUrl = `data:image/svg+xml,${encodeURIComponent(faviconSvg)}`;
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href={faviconDataUrl} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
